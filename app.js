63af41021fee876d9aef11fd0419ca5f3c7f9f86
OmriManor <omri.manor@dynamicyield.com>
2016-03-04 12:19:23 +0200

    allowing cors in reponse

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var mysql = require('mysql');
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var password;
 try {
     password = fs.readFileSync('/home/ec2-user/mysqlcreds', 'utf8').replace(/\n$/, '')
