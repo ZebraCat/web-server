84ea64a4bcc1a3ab42a44d46d4bfb43150fa4756
OmriManor <omri.manor@dynamicyield.com>
2016-06-03 14:58:21 +0300

    black list with country as prefix

--- src/influencer-redis-manager.js
+++ src/influencer-redis-manager.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var BLACK_LIST = 'black_list';
+var DELIMITER = ':';
 var redis = require("redis");
 var client = redis.createClient();
 
@@ -8,7 +9,7 @@ var InfluencerRedisManager = {};
 
 InfluencerRedisManager.deleteInfluencer = function(influencer) {
     client.del(influencer.username);
-    client.sadd(BLACK_LIST, influencer.username)
+    client.sadd(BLACK_LIST, influencer.country + DELIMITER + influencer.username)
 };
 
 module.exports = InfluencerRedisManager;
